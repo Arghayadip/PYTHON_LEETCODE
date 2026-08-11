777d6878113a012972dfa42ac3bcdd62661ce6a7
Arghayadip Tung <tungarghayadip110@gmail.com>
2026-08-11 20:41:00 +0530

    Time: 0 ms (100%), Space: 12.6 MB (38.74%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        pairs = { ')': '(', ']':'[','}':'{'}
+        for char in s:
+            if char in pairs:
+                if len(stack) == 0  or stack[-1] != pairs[char]:
+                    return False
+                stack.pop()
+            else:
+                stack.append(char)
+        return len(stack) == 0
