3b92900110ed440db7a5dc7d09f7eb8b12aee679
Arghayadip Tung <tungarghayadip110@gmail.com>
2026-08-14 22:21:18 +0530

    Time: 4 ms (32.69%), Space: 12.4 MB (59.62%) - LeetHub

--- 3936-minimum-swaps-to-move-zeros-to-end/3936-minimum-swaps-to-move-zeros-to-end.py
+++ 3936-minimum-swaps-to-move-zeros-to-end/3936-minimum-swaps-to-move-zeros-to-end.py
@@ -1,23 +1,17 @@
 class Solution(object):
     def minimumSwaps(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
         swap = 0
-        n = len(nums)
         i = 0
-        j = n-1
-        while i<j:
-            if nums[i] == 0 and nums[j] != 0:
-                nums[i],nums[j] = nums[j], nums[i]
-                swap += 1
+        j = len(nums) - 1
+
+        while i < j:
+            if nums[i] == 0:
+                if nums[j] != 0:
+                    nums[i], nums[j] = nums[j], nums[i]
+                    swap += 1
+                    i += 1
+                j -= 1
+            else:
                 i += 1
-                j -= 1
-            elif nums[i] == 0 and nums[j]== 0:
-                j -= 1
-            elif nums[i] != 0 and nums[j]== 0:
-                j -= 1
-            elif nums[i] != 0 and nums[j] != 0:
-                i+=1
+
         return swap
